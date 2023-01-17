b27564dc2fa178b5470b344ccf90ec622f62374e
hardy <nobat@gmail.com>
2023-01-17 02:56:56 +0000

    tambah floatterm dan multicursor

---
+++ lua/plugins/floatterm.lua
@@ -0,0 +1,13 @@
+return {
+
+	'voldikss/vim-floaterm',
+	lazy= true, 
+	keys ={ 
+	{"<leader><leader>", function() end}
+	},
+	config= function ()
+
+			
+	end
+	
+}
---
+++ lua/plugins/multicursor.lua
@@ -0,0 +1,17 @@
+return {
+
+	{
+
+		'mg979/vim-visual-multi',
+		branch = 'master',
+		lazy = true, 
+		keys = {
+
+			{"<leader><leader>", function()
+
+			end}
+		}
+		
+	}
+	
+}
