b23c7229b6d9de73641b70ae3effa1d189b4d079
hardy <nobat@gmail.com>
2023-01-17 03:07:38 +0000

    tambah floaterm

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -3,7 +3,8 @@ return {
 	'voldikss/vim-floaterm',
 	lazy= true, 
 	keys ={ 
-	{"<leader><leader>", function() end}
+	{"<leader><leader>", function() end},
+	{"ft","<cmd>FloatermToggle<cr>"},	
 	},
 	config= function ()
 
