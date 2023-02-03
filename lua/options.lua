59978d2b2f23ee563dc5922e572e22921e26d535
hardy <hardy@gmail.com>
2023-02-03 18:15:41 +0800

    fix init.lua and option.lua

--- lua/options.lua
+++ lua/options.lua
@@ -19,26 +19,18 @@ vim.o.breakindent = true
 
 --Save undo history
 vim.cmd [[set undofile]]
-
---Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
---Decrease update time
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 
---Set colorscheme (order is important here)
 vim.o.termguicolors = true
 --vim.o.background = 'dark'
 vim.g.onedark_terminal_italics = 2
 
---vim.cmd [[colorscheme default]]
---vim.cmd [[hi NormaFloat guibg=NONE ctermbg=LightGrey]]
 vim.cmd.colorscheme('default')
 
 vim.api.nvim_set_hl(0, "NormalFloat", {bg="none", ctermbg="none"})
 vim.api.nvim_set_hl(0, "Normal", {bg="none", ctermbg="none"})
 vim.api.nvim_set_hl(0, "Pmenu", {bg="#5E5C5B", fg="#B4FF00"})
---vim.api.nvim_set_hl(0, "PmenuSel", {bg="none", fg="none"})
 
